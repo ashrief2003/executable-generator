d7538c4e00fd3a067fe308feeafeca6c8f93facf
ashrief2003 <161985309+ashrief2003@users.noreply.github.com>
2024-09-06 01:57:51 +0200

    Add files via upload

---
+++ sec/core.h
@@ -0,0 +1,14 @@
+#ifndef CORE_H
+#define CORE_H
+
+extern const unsigned char winH[];
+extern unsigned char _data[];
+extern unsigned char _code[];
+extern const unsigned char _idata[];
+
+extern int WinHSize;
+extern int dataSize;
+extern int codeSize;
+extern int idataSize;
+
+#endif
